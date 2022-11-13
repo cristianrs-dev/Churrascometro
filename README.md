9c051e9d9a6c3f4a8667f62d77373e9bb557b623
eclipseCJP <cris.tecwordd@gmail.com>
2022-11-13 10:29:53 -0400

    primeiro commit

---
+++ README.md
@@ -0,0 +1,10 @@
+App simples para calcular a os comes e bebes a partir da quantidade de convidados
+
+Conceitos trabalhados:
+
+html
+*tags de formulario
+
+js
+*funcoes
+*evento onclik
---
+++ js/script.js
@@ -0,0 +1,39 @@
+let inputAdultos=document.getElementById("adultos");
+let inputCriancas=document.getElementById("criancas");
+let inputDuracao=document.getElementById("duracao");
+let resultado=document.getElementById("resultado");
+
+//util para testar o valor do input
+//console.log(inputAdultos.value=6,inputCriancas.value=3,inputDuracao.value=9);
+
+function calcular(){
+  //  console.log("calculando...")
+
+    let adultos=inputAdultos.value;
+    let criancas=inputCriancas.value;
+    let duracao=inputDuracao.value;
+
+
+    let qtdTotalCarne = carnePpessoa(duracao) * adultos + (carnePpessoa(duracao) / 2 * criancas);
+    let qtdTotalBebida = bebidaPpessoa(duracao)*adultos + (bebidaPpessoa(duracao) / 2 * criancas);
+    
+    resultado.innerHTML=`<p>${qtdTotalCarne} kl de carne</p>
+                        <p>${qtdTotalBebida} litros de bebida</p>
+                        `
+}
+
+function carnePpessoa(duracao){
+    let carne = 400;
+    if(duracao >= 6){
+        carne=650;
+    }
+    return carne;
+}
+
+function bebidaPpessoa(duracao){
+    let bebida=1200;
+    if(duracao >= 6){
+        bebida=2000;
+    }
+    return bebida;
+}
